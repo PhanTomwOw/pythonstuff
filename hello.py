1a247eebe39aeeece7f5f819774f31b15c16b8b9
Mehdi Shafizadeh <92962493+PhanTomwOw@users.noreply.github.com>
2026-08-18 06:12:50 +0330

    Initial commit

---
+++ hello.py
@@ -0,0 +1,26 @@
+import os
+os.system('clear')
+
+ ############################
+# DATA TYPES
+# Strings -  Done.
+# Numbers - Done.
+# Lists - Done.
+# Tuples - Done.
+# Dictionaries - Done.
+# Boolean - 
+#############################
+
+
+
+fav_music = {
+	"Mehdi": "Notorious b.i.g",
+	"Masoud": "Kendrick lamar",
+	"Sajjad": "Hichkas"
+}
+ print([fav_music ["Mehdi"]])
+
+
+
+
+ 
